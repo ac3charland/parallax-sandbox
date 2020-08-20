1b350908c3cf53a43620bbf6a74dce5bc450540c
Alex Charland <ac3charland@gmail.com>
2020-08-20 11:59:04 -0500

    Spike basic parallax setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [checked, setChecked] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={checked ? 'debug-on' : ''}>
+      <div className="debug">
+        <label>
+          <input type="checkbox" value={checked} onClick={() => setChecked(!checked)}/>
+          Debug
+        </label>
+      </div>
+      <div className="parallax">
+        <div id="group-1" className="parallax__group mock-navbar">
+          <div class="parallax__layer parallax__layer--base">
+            <div className="title">
+              This is a navbar
+          </div>
+          </div>
+        </div>
+        <div id="group-2" className="parallax__group mock-hero">
+          <div id="bg-image-1" class="parallax__layer parallax__layer--back">
+          </div>
+          <div class="parallax__layer parallax__layer--base">
+            <div className="title">
+              SITE HERO TEXT
+          </div>
+          </div>
+        </div>
+        <div id="group-3" className="parallax__group">
+          <div class="parallax__layer parallax__layer--base">
+            <div className="title">
+              This is the foreground
+          </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
